1652606b6a4f0d8040f2cdf4816af649e72312fc
minari515 <126942302+minari515@users.noreply.github.com>
2023-03-05 10:56:10 +0900

    最初の更新

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
+import React, { Component } from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-import ImageUploader from "./ImageUploader";
+import MovieUploader from "./MovieUploader";
 
-function App() {
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="container text-center mt-5">
+        <Routes>
+            <Route path={`/`} element={<Home />} />
+            <Route path={`/about/`} element={<About />} />
+            <Route path={`/marker/`} element={<Marker />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <p><Link to="/about">始める</Link></p>
+    </div>
+  )
+}
+
+const About = () => {
   return (
     <div className="App">
       {/* uplodaer */}
-      <ImageUploader />
+      <MovieUploader />
     </div>
   );
 }
+
+const Marker = () => {
+  return (
+    <div>
+      <h1>ARマーカー作成</h1>
+      <p><Link to="/about">始める</Link></p>
+    </div>
+  )
+}
+
+// const Marker = () => {
+//   return (
+//     <div className="Marker">
+//       {/* makemarker */}
+//       <Makemarker />
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/MovieUploader.js
@@ -0,0 +1,89 @@
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+import ImageLogo from "./movie.svg";
+import "./MovieUpload.css";
+import storage from "./firebase";
+import { ref, uploadBytesResumable } from "firebase/storage";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+const MovieUploader = () => {
+  const [loading, setLoading] = useState(false);
+  const [isUploaded, setUploaded] = useState(false);
+
+  const OnFileUplodeToFirebase = (e) => {
+    console.log(e.target.files);
+    const file = e.target.files[0]
+    const storageRef = ref(storage, file.lastModified + "/" + file.name);
+    const uploadMovie = uploadBytesResumable(storageRef, file);
+
+    uploadMovie.on(
+      "state_change",
+      (snapshot) => {
+        setLoading(true);
+      },
+      (err) => {
+        console.log(err)
+      },
+      () => {
+        setLoading(false);
+        setUploaded(true);
+        var user_id = file.lastModified
+        console.log(file)
+        var result = document.getElementById("result");
+        let qrValue = "https://" + user_id
+        document.getElementById("qr-code").src = 'https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=' + qrValue;
+      }
+    )
+  };
+  return (
+    <>
+      {loading ? (
+        <h2>アップロード中・・・</h2>
+      ) : (
+        <>
+          {isUploaded ? (
+            <>
+              <div className="qr-code">
+                <img id="qr-code" src="qr-code.png" alt="qr-code" class="qr-code"/><br/>
+              </div>
+              <div className="result">
+                <h2 id="result"><p>アップロード完了しました！</p></h2>
+                <p><Link to="/marker">次へ</Link></p>
+              </div>
+            </>
+          ) : (
+          <div className="outerBox">
+            <div className="title">
+              <h2>動画アップローダー</h2>
+              <p>mp4の画像ファイル</p>
+            </div>
+            <div className="movieUplodeBox">
+              <div className="movieLogoAndText">
+                <img src={ImageLogo} alt="imagelogo" />
+                <p>ここにドラッグ＆ドロップしてください</p>
+              </div>
+              <input className="movieUploadInput" multiple name="movieURL" type="file" accept=".mp4" onChange={OnFileUplodeToFirebase} />
+            </div>
+            <p>または</p>
+            <Button variant="contained">
+              ファイルを選択
+              <input className="movieUploadInput" type="file" onChange={OnFileUplodeToFirebase} accept=".mp4"/>
+            </Button>
+          </div>
+          )}
+        </>
+      )}
+    </>
+  );
+};
+
+const Marker = () => {
+  return (
+    <div>
+      <h1>ARマーカー作成</h1>
+      <p><Link to="/about">始める</Link></p>
+    </div>
+  )
+}
+
+export default MovieUploader;
